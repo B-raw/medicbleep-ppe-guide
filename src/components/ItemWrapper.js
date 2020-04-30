cf391833f057b1f8125c82249cbfd84a4f81f470
B-raw <michaelbarry1989@hotmail.com>
2020-04-30 13:12:44 +0100

    removed console.logs

--- src/components/ItemWrapper.js
+++ src/components/ItemWrapper.js
@@ -2,14 +2,11 @@ import React from 'react';
 import ItemBlock from './ItemBlock';
 
 function ItemWrapper(props) {
-  console.log(props)
-
   return (
     <div>
       <h2>PPE Overview</h2>
-      {/* Iterate over each ppe_item in props and create a ItemBlock */}
+      {/* Iterate over each ppe_item in props and create an ItemBlock */}
       {props.ppe_items.map(item => {
-        console.log(item)
         return <ItemBlock name={item.fields.name} key={item.id}/>
       })}
     </div>
