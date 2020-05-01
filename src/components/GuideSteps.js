ffbc05b5ce96e78814af720c8e4b3c3b4a598191
B-raw <michaelbarry1989@hotmail.com>
2020-05-01 11:41:29 +0100

    added ReactPlayer which now works

--- src/components/GuideSteps.js
+++ src/components/GuideSteps.js
@@ -4,12 +4,17 @@ import ReactPlayer from 'react-player'
 const GuideSteps = ({data}) => {
   const { title, description, video_url, steps } = data.fields
 
+  const canPlay = ReactPlayer.canPlay(video_url)
+  const videoSection = canPlay ? <ReactPlayer url={video_url} playing /> : "Sorry, video cannot be played."
+
   return (
     <div>
       <a href="#">&lt; Back to Overview</a>
       <h1>{title}</h1>
       <p>{description}</p>
-      {/* <ReactPlayer url={video_url} playing /> */}
+      <div>
+        {videoSection}
+      </div>
       <ol>
         {/* First need to sort the order of the steps - order is a string so need to parse to number */}
         {steps.sort((a,b) => {
